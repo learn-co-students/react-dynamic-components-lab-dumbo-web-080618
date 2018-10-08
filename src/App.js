1b1e812b30a07d6ef37a082091a1c3f360d8417b
Madeline Celis <celiscb@vcu.edu>
2018-10-08 15:50:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';  //the curly braces are destructuring
+//which means that inside of the react object you have a key called component
+//when you see the key component just grab it is what that {Component} is saying
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
+import Comment from './Comment.js'
 
 class App extends Component {
-  
-  // Nothing needs to change here! 
+  //extends mean inherit from Component
+  // Nothing needs to change here!
   // Make note of what prop we are initially passing to `ColorBox` below!
-  
+
   render() {
     return (
       <div id="app">
           <BlogPost />
+          <Comment />
           <div id="seperator"></div>
           <div className="wrapper">
             <ColorBox opacity={1} />
